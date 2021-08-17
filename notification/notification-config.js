ee82600184155e05c508f9aad80df4e6e3bb978c
nnnbinh <nambinh2016@gmail.com>
2021-08-17 21:57:40 +0700

    update

--- notification/notification-config.js
+++ notification/notification-config.js
@@ -1,7 +1,7 @@
 var admin = require("firebase-admin");
 
 //thay file firebase sdk
-const serviceAccount = require("./firebase-admin.json");
+const serviceAccount = require("./firebase-sdk.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
